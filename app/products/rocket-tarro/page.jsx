381c8cb8bef47a00c07b28d1fe7ef424cb513cdd
Hugo Corona <hugo.corona.r@gmail.com>
2023-04-22 21:52:54 -0700

    Queda pendiente el mapa de colores

--- app/products/rocket-tarro/page.jsx
+++ app/products/rocket-tarro/page.jsx
@@ -4,10 +4,15 @@ import rocketTarro from "../../../public/tarro_clean.png";
 import rocketTarro2 from "../../../public/demo.png";
 import rocketTarro3 from "../../../public/demo2.png";
 
-import Phrase from '@/app/components/Phrase';
 
 
-
+const colours = [
+  {name:'red', rgb:'e63946'},
+  {name:'blue', rgb:'3a86ff'},
+  {name:'green', rgb:'06d6a0'}, 
+  {name:'yellow', rgb:'ffbe0b'},
+  {name:'porpol', rgb:'8338ec'} 
+  ]
 
 export default function RocketTarroPage ()  {
   return (
@@ -25,7 +30,11 @@ export default function RocketTarroPage ()  {
         
         <div className='grid grid-cols-2'>
           <span className='text-xl'>COLOR DE LA MANIJA:</span>
-          <div class="w-9 h-9 rounded-full bg-blue-500"></div>
+          <div className='flex flex-row gap-2'>
+            {colours.map(colour => <div key={colour.name}  className=''></div>)}
+            
+            
+          </div>
           
         </div>
       </div>
